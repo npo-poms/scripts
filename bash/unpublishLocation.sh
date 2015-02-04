fae886942b6330781915caf0b2f127b80623d1d7
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2015-02-04 12:38:00 +0100

    scripts to unupblish locations in bulk

--- bash/unpublishLocation.sh
+++ bash/unpublishLocation.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -x
+#set -x
 if [ -z "$1" -o -z "$2" ] ; then
     echo use "$0 <source mid> <location id>"
     exit
@@ -14,15 +14,9 @@ else
     publishStop=$3
 fi
 
-curl -s --insecure --user $user --header "Content-Type: application/xml" -X GET  $rs/media/$1/locations | xsltproc  --stringparam id $2 --stringparam publishStop $publishStop location_set_publishStop.xslt -
+curl -s --insecure --user $user --header "Content-Type: application/xml" -X GET  $rs/media/$1/locations | xsltproc  --stringparam id $2 --stringparam publishStop $publishStop location_set_publishStop.xslt - > /tmp/location.xml
 
-exit
-target=$rs/media/$2/location?errors=michiel.meeuwissen@gmail.com
+target=$rs/media/$1/location?errors=michiel.meeuwissen@gmail.com
 
-for i in `ls $tempdir`; do
-    cat $tempdir/$i | xmllint -format - | grep programUrl
-    curl -s --insecure --user $user --header "Content-Type: application/xml" -X POST --data @$tempdir/$i  ${target}
-    echo
-done
-
-rm -rf $tempdir
+cat /tmp/location.xml
+curl -s --insecure --user $user --header "Content-Type: application/xml" -X POST --data @/tmp/location.xml  ${target}
