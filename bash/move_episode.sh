58dbf5a6c3bc4d9477035ca9ba6eacd19be66554
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2015-06-25 11:46:46 +0200

    MSE-2348

---
+++ bash/move_episode.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if [ -z "$1" ] ; then
+    echo use "$0 <id> <from> <to>"
+    exit
+fi
+
+SOURCE=$(readlink  $BASH_SOURCE)
+if [[ -z "$SOURCE" ]] ; then
+    SOURCE=$BASH_SOURCE
+fi
+source $(dirname ${SOURCE[0]})/creds.sh
+source $(dirname ${SOURCE[0]})/functions.sh
+
+
+target=$(getUrl group/$1/episodes?errors=$errors)
+
+echo $target >&2
+curl -s --insecure --user $user --header "Content-Type: application/xml" -X PUT  ${target} -d"
+<move xmlns='urn:vpro:media:update:2009'>
+      <from>$2</from>
+      <to>$3</to>
+</move>"
