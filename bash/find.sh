e0a971cb4f98a843bc7bfb064dc0d47d9162740d
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2015-08-31 14:31:24 +0200

    MSE-2867

--- bash/find.sh
+++ bash/find.sh
@@ -11,6 +11,11 @@ fi
 source $(dirname ${SOURCE[0]})/creds.sh
 source $(dirname ${SOURCE[0]})/functions.sh
 
+writable=false
+
+if [[ ! -z "$2" ]] ; then
+    writable=$2
+fi
 
 curl -s --insecure --user $user --header "Content-Type: application/xml" -X POST --data @$1 \
-    $(getUrl find)  | xmllint -format -
+    $(getUrl find)?writable=$writable
