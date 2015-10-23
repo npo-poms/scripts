f97b459f01e64c4d64e662614a21d46a28f43c2e
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2015-10-23 11:03:03 +0200

    made possible to post url too

--- bash/postimage.sh
+++ bash/postimage.sh
@@ -3,12 +3,7 @@ if [ -z "$1" ] ; then
     echo use "$0 <mid> [<image.xml>]"
     exit
 fi
-
-if [ -z "$2" ] ; then
-    imagexml="image.xml"
-else
-    imagexml=$2
-fi
+#set -x
 
 SOURCE=$(readlink  $BASH_SOURCE)
 if [[ -z "$SOURCE" ]] ; then
@@ -18,11 +13,31 @@ source $(dirname ${SOURCE[0]})/creds.sh
 source $(dirname ${SOURCE[0]})/functions.sh
 
 
+if [ -z "$2" ] ; then
+    imagexml="image.xml"
+else
+    if [ -e "$2" ] ; then
+        imagexml=\@$2
+    else
+        read -r -d '' imagexml << EOF
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<image xmlns="urn:vpro:media:update:2009">
+  <title>Image for $1</title>
+  <imageLocation>
+    <url>$2</url>
+  </imageLocation>
+</image>
+EOF
+    fi
+fi
+
+
+
 target=$(getUrl media/$1/image?errors=$errors)
 
-echo posting $imagexml
+echo posting "$imagexml"
 
-curl   -s --insecure --user $user --header "Content-Type: application/xml" -X POST --data \@$imagexml \
+curl   -s --insecure --user $user --header "Content-Type: application/xml" -X POST --data "$imagexml" \
     ${target}
 error=$?
 if [ $error != 0 ] ; then
