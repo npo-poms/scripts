2961135fdbf829b11997e051dbda1e241a27d7bb
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2015-10-23 11:07:24 +0200

    some (unfinished?) pythong examples

---
+++ python/addGenreTo.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+"""Script to add a genre to all members of a group. Supported in poms >= 3.2 only"""
+import sys
+import poms
+
+def usage():
+    print sys.argv[0] + " [-r] [-h] [-s] [-t <target>] <MID> <genreId>"
+
+def main():
+
+    opt, args = poms.opts(usage = usage, minargs = 2)
+    mid, genre_id = args
+
+    update = poms.get(mid)
+
+    print("Adding genre " + genre_id + " to " + mid);
+    poms.add_genre(update, genre_id)
+    poms.post(update)
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ python/members.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+"""Get's and formats a POMS media update xml"""
+import sys
+import poms
+from xml.dom import minidom
+
+
+def usage():
+    print(sys.argv[0] +
+          " [-r] [-h] [-t <target>] <MID>")
+
+def main():
+    opts, args = poms.opts(usage = usage, minargs = 1, args="t:srh")
+    mid  = args[0]
+    print poms.members(mid);
+
+
+if __name__ == "__main__":
+    main()
