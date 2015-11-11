bf7078e6ddec9872ca56676a98013733cceb7a56
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2015-11-11 10:15:47 +0100

    a bit silly to have a main method

--- python/get.py
+++ python/get.py
@@ -7,24 +7,27 @@ import xml.etree.ElementTree as ET
 
 def usage():
     print(sys.argv[0] +
-          " [-r] [-h] [-t <target>] <MID> [<xpath>]")
+          " [-r] [-h] [-t <target>] <MID> [<xpath>] [<attr>]")
 
-def main():
+
+if __name__ == "__main__":
     opts, args = poms.opts(usage = usage, minargs = 1, args="t:srh")
     mid  = args[0]
     xpath = None
     if len(args) > 1:
         xpath = args[1]
+
     if xpath:
+        attr = None
+        if len(args) > 2:
+            attr = args[2]
+
         result = poms.get(mid, parser=ET).findall(xpath, poms.namespaces)
-        if len(result) > 0:
-            print(result[0].text)
-        else:
-            print(result)
+        for el in result:
+            if attr:
+                print(el.get(attr))
+            else:
+                print(el.text)
+
     else:
         print(poms.get(mid, parser=minidom).toprettyxml(encoding='utf-8', indent='  ').decode(sys.stdout.encoding, "surrogateescape"))
-
-
-
-if __name__ == "__main__":
-    main()
