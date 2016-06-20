39117247e5256d9298ac665d67214897d9cb89e5
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2016-06-20 10:54:47 +0200

    Details.

--- python/cleanupGroup.py
+++ python/cleanupGroup.py
@@ -3,7 +3,6 @@
 This script checks wether a poms playlist contains multiple translations of the same source. If so, it will delete remove all but one.
 """
 from npoapi import MediaBackend
-import pprint
 
 api = MediaBackend().command_line_client()
 api.add_argument('mid', type=str, nargs=1, help='The mid  of the object to handle')
@@ -12,7 +11,7 @@ args = api.parse_args()
 group = args.mid[0]
 members = api.members(group, max=3000, batch=50)
 
-mids = {}
+translations_for_mid = {}
 for member in members:
     update = member.firstChild
     mid = update.attributes["mid"].value
@@ -21,15 +20,15 @@ for member in members:
         relation_type = relation.attributes["type"].value
         if relation_type == "TRANSLATION_SOURCE":
             translation_source = relation.firstChild.nodeValue
-            if not translation_source in mids:
-                mids[translation_source] = []
-            mids[translation_source].append(mid)
+            if not translation_source in translations_for_mid:
+                translations_for_mid[translation_source] = []
+            translations_for_mid[translation_source].append(mid)
 
 #print(pprint.pformat(map))
-for mid in mids:
-    mid_to_delete = sorted(mids[mid])[1:]
+for mid in translations_for_mid:
+    mid_to_delete = sorted(translations_for_mid[mid])[1:]
     if len(mid_to_delete) > 0:
-        print("for %s are the following translations %s , to delete are %s" % (mid, str(mids[mid]), str(mid_to_delete)))
+        print("for %s are the following translations %s , to delete are %s" % (mid, str(translations_for_mid[mid]), str(mid_to_delete)))
         for d in mid_to_delete:
             print("Deleting %s from %s" % (d, group))
             api.delete_member(d, group)
