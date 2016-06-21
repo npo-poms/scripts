992b7dbbb001a3108a8e5c64911a9e53721ae9a3
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2016-06-21 10:45:33 +0200

    details

--- python/cleanupGroup.py
+++ python/cleanupGroup.py
@@ -9,7 +9,7 @@ api.add_argument('mid', type=str, nargs=1, help='The mid  of the object to handl
 args = api.parse_args()
 
 group = args.mid[0]
-members = api.members(group, max=3000, batch=50)
+members = api.members(group, batch=200)
 
 translations_for_mid = {}
 for member in members:
@@ -26,11 +26,13 @@ for member in members:
 
 #print(pprint.pformat(map))
 for mid in translations_for_mid:
-    mids_to_delete = sorted(translations_for_mid[mid])[1:]
+    sorted_mids = sorted(translations_for_mid[mid])
+    mids_to_delete = sorted_mids[1:]
+    mid_to_keep = sorted_mids[0]
     if len(mids_to_delete) > 0:
         print("for %s are the following translations %s , to delete are %s" % (mid, str(translations_for_mid[mid]), str(mids_to_delete)))
         for d in mids_to_delete:
-            print("Deleting %s from %s" % (d, group))
+            print("Deleting %s from %s (keeping %s)" % (d, group, mid_to_keep))
             api.delete_member(d, group)
 
 
