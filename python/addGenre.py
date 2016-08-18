c2a4f618c32dbf39db1cdd3016e3caa697224cf0
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2016-08-18 14:43:53 +0200

    migrated to pyapi

--- python/addGenre.py
+++ python/addGenre.py
@@ -1,26 +1,31 @@
 #!/usr/bin/env python3
 """Script to add a genre to all members of a group. Supported in poms >= 3.2 only"""
 import sys
-import poms
 
-def usage():
-    print sys.argv[0] + " [-r] [-h] [-s] [-t <target>] <MID> <genreId>"
+from npoapi import MediaBackend
+from npoapi.xml import mediaupdate
+from npoapi.xml import poms
 
-def main():
+api = MediaBackend().command_line_client()
+api.add_argument('mid', type=str, nargs=1, help='The mid  of the object to handle')
+api.add_argument('genre', type=str, nargs=1, help='new Genre')
+args = api.parse_args()
 
-    opt, args = poms.opts(usage = usage, minargs = 2)
-    mid, genre_id = args
+genre_id = args.genre[0]
 
-    for s in poms.members(mid):
-        #print s.attributes['position'].value
-        update     = s.getElementsByTagName('mediaUpdate')[0];
-        member_mid = update.attributes['mid'].value
+bytes = api.get(args.mid[0], ignore_not_found=True)
+if bytes:
+    object = poms.CreateFromDocument(bytes)
+    if type(object) == mediaupdate.programUpdateType:
+        object.genre.append(genre_id)
+        api.post(object)
 
-        print("Adding genre " + genre_id + " to " + member_mid);
-        poms.add_genre(update, genre_id)
-        poms.post(update)
+    members = api.members(args.mid[0], batch=200)
+
+    for member in map(lambda m: poms.CreateFromDOM(m.getElementsByTagName("mediaUpdate")[0], mediaupdate.Namespace), members):
+        member_mid = member.mid
+        print("Adding genre " + genre_id + " to " + member_mid)
+        member.genre.append(genre_id)
+        api.post(member)
 
 
-
-if __name__ == "__main__":
-    main()
