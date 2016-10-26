6248d14e724cdaa88cc88458a923d4c74bb53bab
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2016-10-26 11:52:08 +0200

    migrated to pyapi

--- python/addLocation.py
+++ python/addLocation.py
@@ -1,19 +1,14 @@
 #!/usr/bin/env python3
 """ """
-import sys
-import poms
+"""Script to add a location """
 
-def usage():
-    print(sys.argv[0] + " [-r] [-h] [-s] [-t <target>] <MID> <program url>")
+from npoapi import MediaBackend, MediaBackendUtil as MU
 
-def main():
+api = MediaBackend().command_line_client()
+api.add_argument('mid', type=str, nargs=1, help='The mid  of the object to handle')
+api.add_argument('location', type=str, nargs=1, help='URL of the new "location"')
 
-    opt, args = poms.opts(usage = usage, minargs = 2)
-    mid, program_url = args
+args = api.parse_args()
 
-    poms.post_location(mid, program_url)
+print(api.add_location(args.mid[0], MU.create_location(args.location[0])))
 
-
-
-if __name__ == "__main__":
-    main()
