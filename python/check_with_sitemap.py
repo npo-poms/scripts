4b639c1d442830a4aab71afe731498a91f84642d
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2017-01-13 11:18:10 +0100

    do mapping afterwards

--- python/check_with_sitemap.py
+++ python/check_with_sitemap.py
@@ -6,8 +6,8 @@ It will download the entire profile from the NPO Front end api, and it will also
 
 Then, it compares the found URL's in both. They should represent the same set.
 
-If there are URL in the API which are not in the Sitemap, which are indeed not existing (give 404's) then the script supposes this is
-an error and deletes the object from the API.
+If there are URL in the API which are not in the Sitemap, which are indeed not existing (give 404's) then the script
+supposes this is an error and deletes the object from the API.
 
 If objects are in the API but not in the sitemap, then we suppose the sitemap is outdated.
 
@@ -26,8 +26,8 @@ import pickle
 import os
 import urllib
 import xml.etree.ElementTree
-
-
+import re
+import codecs
 
 api = Pages().command_line_client()
 backend = PagesBackend(env=api.actualenv).configured_login()
@@ -35,6 +35,7 @@ api.add_argument('sitemap', type=str, nargs=1, help='sitemap')
 api.add_argument('profile', type=str, nargs='?', help='profile')
 api.add_argument('-C', '--clean', action='store_true', default=False, help='clean build')
 api.add_argument('-D', '--delete', action='store_true', default=False, help='delete from api')
+api.add_argument('--https_to_http', action='store_true', default=False, help='Replace all https with http')
 
 args = api.parse_args()
 
@@ -42,39 +43,48 @@ profile = args.profile
 sitemap_url = args.sitemap[0]
 clean = args.clean
 delete_from_api = args.delete
+https_to_http = args.https_to_http
 
 if clean:
     api.logger.info("Cleaning")
 
+
 def get_urls_from_api() -> set:
     offset = 0
-    urls = set()
+    new_urls = set()
     while True:
         result = api.search(profile=profile, offset=offset, limit=240)
         json_object = json.loads(result)
         items = json_object['items']
         total = json_object['total']
-        api.logger.info("API: Found %s/%s urls", len(urls), total)
+        api.logger.info("API: Found %s/%s urls for profile %s", len(new_urls), total, profile)
         grow = 0
         for item in items:
             url = item['result']['url']
-            urls.add(url)
+            if https_to_http:
+                url = re.sub(r'^https://(.*)', r'http://\1', url)
+            new_urls.add(url)
             offset += 1
             grow += 1
 
         if grow == 0:
             break
-    return urls
+    return new_urls
 
 
 def get_urls() -> set:
     url_file = "/tmp/urls." + profile + ".p"
     if os.path.exists(url_file) and not clean:
-        urls = pickle.load(open(url_file, "rb"))
+        new_urls = pickle.load(open(url_file, "rb"))
     else:
-        urls = get_urls_from_api()
-        pickle.dump(urls, open(url_file, "wb"))
-    return urls
+        new_urls = sorted(get_urls_from_api())
+        pickle.dump(new_urls, open(url_file, "wb"))
+
+    with codecs.open(profile + ".txt", 'w', "utf-8") as f:
+        f.write('\n'.join(list(new_urls)))
+    api.logger.info("Wrote %s", profile + ".txt")
+
+    return new_urls
 
 
 def get_sitemap_from_xml():
@@ -84,27 +94,35 @@ def get_sitemap_from_xml():
         if el.tag == "{http://www.sitemaps.org/schemas/sitemap/0.9}loc":
             locs.add(el.text)
     response.close()
-    urls = set()
+    new_urls = set()
     for loc in locs:
         print(loc)
         response = urllib.request.urlopen(loc)
         for ev, el in xml.etree.ElementTree.iterparse(response):
             if el.tag == "{http://www.sitemaps.org/schemas/sitemap/0.9}loc":
-                urls.add(el.text)
-                if len(urls) % 1000 == 0:
-                    api.logger.info("Sitemap: %s urls", len(urls))
+                url = el.text
+                new_urls.add(url)
+                if len(new_urls) % 1000 == 0:
+                    api.logger.info("Sitemap: %s urls", len(new_urls))
         response.close()
-    return urls
+
+
+    return new_urls
 
 
 def get_sitemap():
     sitemap_file = "/tmp/" + profile + ".sitemap.p"
     if os.path.exists(sitemap_file) and not clean:
-        urls = pickle.load(open(sitemap_file, "rb"))
+        new_urls = pickle.load(open(sitemap_file, "rb"))
     else:
-        urls = get_sitemap_from_xml()
-        pickle.dump(urls, open(sitemap_file, "wb"))
-    return urls
+        new_urls = sorted(get_sitemap_from_xml())
+        pickle.dump(new_urls, open(sitemap_file, "wb"))
+
+    with codecs.open(profile + ".sitemap.txt", 'w', "utf-8") as f:
+        f.write('\n'.join(list(new_urls)))
+    api.logger.info("Wrote %s", profile + ".sitemap.txt")
+
+    return new_urls
 
 def http_status(url):
     try:
@@ -128,10 +146,11 @@ def clean_from_api(urls:set, sitemap:set):
                 api.logger.info("In api, not in sitemap, but not giving 404 url %s: %s", url, str(page.lastPublished))
                 # print(url, http_status(url))
 
+
 def add_to_api(urls:set, sitemap:set):
     print("in sitemap but not in api: %s" % len(sitemap - urls))
-    with open('in_' + profile + "_but_not_in_sitemap.txt", 'w') as f:
-        f.write('\n'.join(list(sitemap - urls)))
+    with open('in_sitemap_but_not_in_' + profile + ".txt", 'w') as f:
+        f.write('\n'.join(sorted(list(sitemap - urls))))
 
     print("Wrote to %s" % f.name)
     for url in list(sitemap - urls)[:10]:
@@ -142,9 +161,17 @@ def add_to_api(urls:set, sitemap:set):
             page = poms.CreateFromDocument(from_backend)
 
 
-urls    = get_urls()
+urls = get_urls()
 sitemap = get_sitemap()
-clean_from_api(urls, sitemap)
+
+if https_to_http:
+    mapped_urls = set(map(lambda url: re.sub(r'^https://(.*)', r'http://\1', url), urls))
+    mapped_sitemap = set(map(lambda url: re.sub(r'^https://(.*)', r'http://\1', url), sitemap))
+else:
+    mapped_urls = urls
+    mapped_sitemap = sitemap
+
+clean_from_api(mapped_urls, mapped_sitemap)
 add_to_api(urls, sitemap)
 
 
