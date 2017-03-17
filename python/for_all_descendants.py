8c4e7dd9cf330afa71f3e4aab2aff9cf681925a5
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2017-03-17 14:48:16 +0100

    made extendibl

--- python/for_all_descendants.py
+++ python/for_all_descendants.py
@@ -3,82 +3,115 @@
 
 import pickle
 import os
+import sys
 from tempfile import gettempdir
 
 # some imports handy for the exec call
+# noinspection PyUnresolvedReferences
 import npoapi.xml.mediaupdate
+# noinspection PyUnresolvedReferences
 from dateutil.parser import parse
 from npoapi import MediaBackend, MediaBackendUtil as MU
 
+def function_or_arg(given_function, args, attr, log):
+    if given_function is None:
+        to_exec = getattr(args, attr)
+        if type(to_exec) == list:
+            to_exec = to_exec[0]
+        if not to_exec is None:
+            if os.path.exists(to_exec):
+                log.info("%s is a file.", str(to_exec))
+                to_exec = open(to_exec).read()
+
+            def exec_string(member, idx):
+                exec(to_exec)
+
+            given_function = exec_string
+
+        else:
+            given_function = None
+    return given_function
+
 api = MediaBackend().command_line_client()
-api.add_argument('mid', type=str, nargs=1, help='The mid  of the object to handle')
-api.add_argument('process', type=str, nargs=1, help="""
-python code to postprocess. E.g.
+
+def main(processor=None, filter=None):
+    api = MediaBackend().command_line_client()
+    api.add_argument('mid', type=str, nargs=1, help='The mid  of the object to handle')
+    if processor is None:
+        api.add_argument('process', type=str, nargs=1, help="""
+    python code to postprocess. E.g.
 
 member.genre.append('1.3.4')
 
 member.publishStop=parse("2018-12-31")
 
 """)
-api.add_argument('-C', '--clean', action='store_true', default=False)
-api.add_argument('--dryrun', action='store_true', default=False)
+    api.add_argument('-C', '--clean', action='store_true', default=False)
+    api.add_argument('--dryrun', action='store_true', default=False)
 
-api.add_argument('--filter', type=str, help="""
+    if filter is None:
+        api.add_argument('--filter', type=str, help="""
 Filter. A piece of python code to filter. E.g.
 type(member) == npoapi.xml.mediaupdate.programUpdateType
 member.type == 'CLIP'
 'kort!' in short_title.lower()
 """)
-args = api.parse_args()
-process = args.process[0]
-mid = args.mid[0]
-filter = args.filter
-clean = args.clean
+    args = api.parse_args()
+    log = api.logger
+    processor = function_or_arg(processor, args, "process", log)
+    mid = args.mid[0]
 
-log = api.logger
+    filter = function_or_arg(filter, args, "filter", log)
+    clean = args.clean
 
-cache = os.path.join(gettempdir(), "foralldescendants." + mid + ".p")
-members = []
-if os.path.exists(cache) and not clean:
-    log.info("Reusing from " + cache)
-    members = pickle.load(open(cache, "rb"))
-else:
-    if not clean:
-        log.info("Not found " + cache)
-    MU.descendants(api, mid, batch=200, target=members, segments=False, episodes=False, log_progress=True)
-    pickle.dump(members, open(cache, "wb"))
+    log = api.logger
 
-log.info("Found " + str(len(members)) + " objects")
-posts = 0
 
-if os.path.exists(process):
-    log.info("%s is a file.", str(process))
-    process = open(process).read()
+    tempfilename = os.path.splitext(os.path.basename(sys.argv[0]))[0]
 
-for idx, member in enumerate(MU.iterate_objects(members)):
-    member_mid = member.mid
-    main_title = MU.title(member, "MAIN")
-    short_title = MU.title(member, "SHORT")
-    if filter:
-        result = eval(filter)
-        log.debug("%s Execed %s result %s", str(idx), str(filter), str(result))
-        if not result:
-            log.debug("Skipping %s, %s %s because of filter %s", str(type(member)), str(member.type), member_mid,
-                            filter)
-            continue
-
-    exec(process)
-
-    if not args.dryrun:
-        log.info("%s Execed %s for %s and posting", str(idx), process, member_mid)
-        api.post(member)
+    cache = os.path.join(gettempdir(), tempfilename + "." + mid + ".p")
+    members = []
+    if os.path.exists(cache) and not clean:
+        log.info("Reusing from " + cache)
+        members = pickle.load(open(cache, "rb"))
     else:
-        log.info("%s Execed %s for %s (not posting because of dryrun parameter)", str(idx), process, member_mid)
+        if not clean:
+            log.info("Not found " + cache)
+        MU.descendants(api, mid, batch=200, target=members, segments=False, episodes=False, log_progress=True)
+        pickle.dump(members, open(cache, "wb"))
 
-    posts += 1
+    log.info("Found " + str(len(members)) + " objects")
+    posts = 0
 
-log.info("Ready. Posted %s updates to POMS", str(posts))
 
+    for idx, member in enumerate(MU.iterate_objects(members)):
+        member_mid = member.mid
+        # noinspection PyUnusedLocal
+        main_title = MU.title(member, "MAIN")
+        # noinspection PyUnusedLocal
+        short_title = MU.title(member, "SHORT")
+        if filter:
+            result = filter(member, idx)
+            log.debug("%s Execed %s result %s", str(idx), str(filter), str(result))
+            if not result:
+                log.debug("Skipping %s, %s %s because of filter %s", str(type(member)), str(member.type) if hasattr(member, "type") else '?', member_mid, filter)
+                continue
+
+        processor(member, idx)
+
+        if not args.dryrun:
+            log.info("%s Execed %s for %s and posting", str(idx), processor, member_mid)
+            api.post(member)
+        else:
+            log.info("%s Execed %s for %s (not posting because of dryrun parameter)", str(idx), processor, member_mid)
+
+        posts += 1
+
+    log.info("Ready. Posted %s updates to POMS", str(posts))
+
+
+if __name__ == "__main__":
+    main()
 
 
 
