15d746a610f5ca771fc2a569de62ce8d0174d231
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2017-04-11 17:01:15 +0200

    added more command line arguments

--- python/for_all_descendants_replace_image.py
+++ python/for_all_descendants_replace_image.py
@@ -17,8 +17,8 @@ def add_image(member, idx):
             new_image = image
     if not new_image:
         new_image = MU.create_image(image_file)
-
-    MU.set_image_fields(new_image, title=image_title, source="http://www.vpro.nl/nooitmeerslapen.html", source_name="VPRO", credits="VPRO", image_type="LOGO")
+    print(image_type)
+    MU.set_image_fields(new_image, title=image_title, source=image_source, source_name=image_source_name, credits=image_credits, image_type=image_type)
 
     member.images.image.append(new_image)
 
@@ -32,12 +32,25 @@ if __name__ == "__main__":
     for_all_descendants.init(add_image, filter_image)
     for_all_descendants.api.add_argument("image_file", type=str, nargs=1, help='image file name')
     for_all_descendants.api.add_argument("image_title", type=str, nargs=1, help='image title')
+    for_all_descendants.api.add_argument("--image_credits", type=str, help='image credits')
+    for_all_descendants.api.add_argument("--image_source_name", type=str,  help='image source name')
+    for_all_descendants.api.add_argument("--image_source", type=str, help='image source')
+    for_all_descendants.api.add_argument("--image_type", type=str, help='image type')
 
+    args = for_all_descendants.api.parse_args()
     global image_file
-    image_file = for_all_descendants.api.parse_args().image_file[0]
+    image_file = args.image_file[0]
     global image_title
-    image_title = for_all_descendants.api.parse_args().image_title[0]
-
+    image_title = args.image_title[0]
+    global image_credits
+    image_credits = args.image_credits
+    print(image_credits)
+    global image_source_name
+    image_source_name = args.image_source_name
+    global image_source
+    image_source = args.image_source
+    global image_type
+    image_type = args.image_type
 
     for_all_descendants.main(add_image, filter_image)
 
