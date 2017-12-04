69ee0ee3eec5f5e6bf32c570d727266575fcecb2
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2017-12-04 15:44:05 +0100

    MGNL-15570

---
+++ python/vpro/clean_from_es.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+
+rawurlencode() {
+  local string="${1}"
+  local strlen=${#string}
+  local encoded=""
+  local pos c o
+
+  for (( pos=0 ; pos<strlen ; pos++ )); do
+     c=${string:$pos:1}
+     case "$c" in
+        [-_.~a-zA-Z0-9] ) o="${c}" ;;
+        * )               printf -v o '%%%02x' "'$c"
+     esac
+     encoded+="${o}"
+  done
+  echo "${encoded}"    # You can either set a return variable (FASTER)
+  REPLY="${encoded}"   #+or echo the result (EASIER)... or both... :p
+}
+
+
+while read url; do
+   curl -X DELETE http://localhost:9208/apipages-publish/page/$(rawurlencode $url)
+done < $1
