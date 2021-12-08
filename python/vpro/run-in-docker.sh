ce569b2e2d1e0872cbe759ecbd86f3f3e494ec28
Michiel Meeuwissen <git@meeuw.org>
2021-12-08 15:01:32 +0100

    mount results so they don't get lost

--- python/vpro/run-in-docker.sh
+++ python/vpro/run-in-docker.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 
-docker run -v ~/conf:/root/conf -v $(cd ..; pwd):/build -w /build/vpro --entrypoint /usr/bin/make mihxil/npo-pyapi-make:latest
+docker run -v ~/conf:/root/conf -v $HOME/integrity-check-results:/root/integrity-check-results -v $(cd ..; pwd):/build -w /build/vpro --entrypoint /usr/bin/make mihxil/npo-pyapi-make:latest
