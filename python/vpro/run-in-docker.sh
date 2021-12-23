c595536a2cbbb1c4f964a62b1539b1c3a0e553ff
Michiel Meeuwissen <git@meeuw.org>
2021-12-23 12:14:35 +0100

    Arrange docker setup too.

--- python/vpro/run-in-docker.sh
+++ python/vpro/run-in-docker.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
+#set -x
+image=docker-proxy.vpro.nl/mihxil/npo-vpro-check:latest
+mounts="-v $HOME/conf:/root/conf -v $HOME/.ssh:/root/.ssh -v $HOME/integrity-check-results:/root/integrity-check-results"
 
-
-docker run -v ~/conf:/root/conf -v $HOME/integrity-check-results:/root/integrity-check-results -v $(cd .. ; pwd):/build -w /build/vpro --entrypoint /usr/bin/make mihxil/npo-pyapi-make:latest ${1:-all}
+docker pull ${image}
+docker run -it ${mounts} --entrypoint /usr/bin/make $image ${1:-all} jmx_binary=/root/jmxterm.jar tunnel=true
+#docker run -it ${mounts}  $image
