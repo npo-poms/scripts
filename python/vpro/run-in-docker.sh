d2df6e0e9fa935c1034c229c6a65421716b44908
Michiel Meeuwissen <git@meeuw.org>
2021-12-23 12:35:06 +0100

    Finishing docker image.

--- python/vpro/run-in-docker.sh
+++ python/vpro/run-in-docker.sh
@@ -5,5 +5,5 @@ export VPRO_CHECK_IMAGE=mihxil/npo-vpro-check:latest
 export VPRO_CHECK_MOUNTS="-v $HOME/conf:/root/conf -v $HOME/.ssh:/root/.ssh -v $HOME/integrity-check-results:/root/integrity-check-results"
 
 docker pull ${VPRO_CHECK_IMAGE}
-docker run -it ${VPRO_CHECK_MOUNTS} --entrypoint /usr/bin/make $image ${1:-all} tunnel=true
+docker run -it ${VPRO_CHECK_MOUNTS} ${VPRO_CHECK_IMAGE} ${1:-all} tunnel=true
 #docker run -it ${mounts}  $image
