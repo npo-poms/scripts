4081d9eba0c6d207b6d8c9c0c69024222a4e2936
Michiel Meeuwissen <git@meeuw.org>
2022-11-16 23:17:47 +0100

    More details.

--- python/vpro/create_json.py
+++ python/vpro/create_json.py
@@ -12,11 +12,25 @@ directory= sys.argv[1] if len(sys.argv) > 1 else "/home/michiel"
 svgs = []
 files = sorted(Path(directory + "/plots").iterdir(), key=os.path.getmtime, reverse=True)
 for file in files:
-    if file.suffix == '.svg': 
+    if file.suffix == '.svg':    
         print(file.name)
+        split = file.stem.split('.', 2)
+        profile = split[0] if len(split) == 2 else None
+        
+        if profile:
+            not_in_api = directory + "/" + profile + ".not-in-api.data"
+            not_in_sitemap = directory + "/" + profile + ".not-in-sitemap.data"
+        else:
+            not_in_api = None
+            not_in_sitemap = None
+        
         ob = {
-           "title": file.name,
-           "svg": "plots/" + file.name
+           "file_name": file.name,
+           "profile": profile,
+           "svg": "plots/" + file.name,
+           "not_in_api":  not_in_api if not_in_api and os.path.exists(not_in_api) else None,
+           "not_in_sitemap":  not_in_sitemap if not_in_sitemap and os.path.exists(not_in_sitemap) else None
+
         }
         svgs.append(ob)
 
