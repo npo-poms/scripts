a39236d4cbf89b6e43ecfca15041b3d066c36303
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2023-11-24 15:04:20 +0100

    Changd script to be able to just fixt the streaming status url.

--- python/migrate_to_cdn/audio_to_cdn.py
+++ python/migrate_to_cdn/audio_to_cdn.py
@@ -8,7 +8,7 @@ from dataclasses import asdict
 
 import requests
 from npoapi import MediaBackend, Binding
-from npoapi.data import ProgramTypeEnum, AvTypeEnum
+from npoapi.data import ProgramTypeEnum, AvTypeEnum, PredictionUpdateType, Prediction
 from xsdata.formats.dataclass.serializers import JsonSerializer
 
 stop = '2023-11-01T12:00:00Z'
@@ -56,40 +56,79 @@ class Process:
         else:
             return False
 
+    def remove_legacy_list(self, mid: str, overview:list):
+        for l in overview:
+            pu = l.split(" ")[0]
+            if "radiobox" in pu or "content.omroep.nl" in pu:
+                self.remove_legacy(mid, pu)
+
     def process_csv(self):
         total = 0
         skipped = 0
         ok = 0
+        corrected = 0
         with (open("SYS-1258.csv", "r", encoding="utf_8") as file):
             reader = csv.DictReader(file, delimiter="\t")
             for row in reader:
                 self.logger.info("Processing %s" % (row))
                 mid = row['mid']
                 total += 1
-                if self.streaming_status(mid):
-                    skipped += 1
-                    self.logger.info("Already online %s" % (mid))
-                    continue
+
                 programurl = row['programurl']
                 if not programurl.endswith(".mp3"):
                     skipped += 1
                     self.logger.info("Not mp3 %s: %s" % (mid, programurl))
                     continue
-                dest = self.download_file(programurl, mid)
-                if dest is None:
-                    self.logger.info("Not downloadla %s" % (programurl))
+
+                workflow = row['workflow']
+                if not workflow == "PUBLISHED":
+                    self.logger.info("Not published %s" % (mid))
                     skipped += 1
                     continue
-                overview = eval(row['onlinelocationurloverview'])
-                self.upload(mid, 'audio/mp3', dest)
 
-                for l in overview:
-                    pu = l.split(" ")[0]
-                    if "radiobox" in pu or "content.omroep.nl" in pu:
-                        self.remove_legacy(mid, programurl)
+                overview = eval(row['onlinelocationurloverview'])
+                if self.streaming_status(mid):
+                    full = self.api.get_full_object(mid)
+                    locations = full.locations.location
+                    found_entry = False
+                    for location in locations:
+                        if location.programUrl.startswith("https://entry"):
+                            found_entry = True
+                    if found_entry:
+                        skipped += 1
+                        self.logger.info("Already online %s" % (mid))
+                        continue
+                    else:
+                        self.logger.info("Online, but missing entry %s" % (mid))
+                        existing_prediction  = full.prediction[0] if len(full.prediction) else  None
+                        if existing_prediction is None:
+                            self.logger.info("No prediction %s" % (mid))
+                            skipped += 1
+                            continue
+                        else:
+                            prediction = Prediction()
+                            prediction.publishStart = existing_prediction.publishStart
+                            prediction.publishStop = existing_prediction.publishStop
+                            prediction.value = "INTERNETVOD"
+                            result = self.api.post_prediction(mid, prediction)
+                            self.logger.info("posted prediction %s-> %s" % (mid, result))
+                            self.remove_legacy_list(mid, overview)
+                            corrected += 1
+                            continue
+
+
+                dest = self.download_file(programurl, mid)
+                if dest is None:
+                    self.logger.info("Not downloadable %s" % (programurl))
+                    skipped += 1
+                    continue
+
+
+                self.upload(mid, 'audio/mp3', dest)
+                self.remove_legacy_list(mid, overview)
                 os.remove(dest)
                 ok += ok
-        self.logger.info("Total %d skipped %d ok %d" % (total, skipped, ok))
+        self.logger.info("Total %d skipped %d ok %d corrected %d" % (total, skipped, ok, corrected))
 
 
 
