707f1374a46750e6a2bc881567be6fedb05d322c
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2024-05-31 00:09:14 +0200

    lowered rate

--- python/migrate_to_cdn/mse5771.py
+++ python/migrate_to_cdn/mse5771.py
@@ -21,11 +21,11 @@ stop = '2023-11-01T12:00:00Z'
 class Process:
 
 
-    def __init__(self, remove_files = True, start_at = 1, progress="mse5771.json", dry_run=False, no_download=False, force_ss=False):
+    def __init__(self, remove_files = True, start_at = 1, progress="mse5771.json", dry_run=False, no_download=False, force_ss=False, endure_seconds=30):
         self.api = MediaBackend().env('prod').command_line_client()
         self.logger = self.api.logger
         self.index = 0
-        self.srcs_endure = timedelta(seconds=30)
+        self.srcs_endure = timedelta(seconds=endure_seconds)
         self.last_upload = datetime.fromtimestamp(0)
         self.logger.info("Talking to %s" % (str(self.api)))
         self.remove_files = remove_files
